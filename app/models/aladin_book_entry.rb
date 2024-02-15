397dc492d0cf0413027d448091c881bba653c687
malkoG <rijgndqw012@gmail.com>
2024-02-16 02:24:25 +0900

    Add import method to AladinBookEntry class to import raw data and create new entries if they don't already exist

--- app/models/aladin_book_entry.rb
+++ app/models/aladin_book_entry.rb
@@ -1,9 +1,38 @@
-require 'aladin_client'
+class AladinBookEntry < ApplicationRecord
+  def self.import(raw_data)
+    return if already_exists?(raw_data)
 
-class AladinBookEntry
-  attr_accessor :book_information
+    hash = { 
+      item_id: "itemId",
+      isbn: 'isbn',
+      isbn13: 'isbn13',
 
-  def initialize(book_information)
-    @book_information = book_information
+      published_at: 'pubDate',
+      
+      title: 'title',
+      link: 'link',
+      author: 'author',
+      publisher: 'publisher',
+
+      description: 'description'
+    }
+
+    options = {}
+    hash.each do |key, label|
+      value = raw_data[label]
+      options[key] = value
+    end
+
+    create!(**options)
+  end
+
+  def self.already_exists?(raw_data)
+    isbn = raw_data['isbn']
+    isbn13 = raw_data['isbn13']
+    item_id = raw_data['itemId']
+
+    find_by(isbn:).present? ||
+      find_by(isbn13:).present? ||
+      find_by(item_id:).present?
   end
 end
