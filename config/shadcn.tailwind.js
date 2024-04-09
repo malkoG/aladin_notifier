fb39f4b06361058ae25099ebb3915b667aaa10d2
malkoG <rijgndqw012@gmail.com>
2024-04-09 11:44:55 +0900

    Disable tailwind-animate

--- config/shadcn.tailwind.js
+++ config/shadcn.tailwind.js
@@ -93,6 +93,6 @@ module.exports = {
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/container-queries"),
-    require("tailwindcss-animate"),
+    // require("tailwindcss-animate"),
   ],
 };
