ea6b635295c118d60679b72a7af53bc1968d4b02
malkoG <rijgndqw012@gmail.com>
2024-04-25 07:56:10 +0900

    Add URL shortening functionality to UrlAlias model

--- app/models/url_alias.rb
+++ app/models/url_alias.rb
@@ -1,2 +1,20 @@
+require "securerandom"
+
 class UrlAlias < ApplicationRecord
+  def self.generate_hash
+    SecureRandom.alphanumeric(6)
+  end
+
+  def self.shorten_url(url)
+    short = ''
+
+    while true
+      short = generate_hash
+      instance = find_by(short:)
+
+      break instance.nil?
+    end
+
+    create!(short:, long: url)
+  end
 end
