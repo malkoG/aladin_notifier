f6de1d48bad2578d3695deeaa802afc31566e240
malkoG <rijgndqw012@gmail.com>
2024-12-01 20:34:14 +0900

    feat: Add route for dynamic page paths in routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
   resources :books
   resources :url_aliases, as: :shortened_redirections, path: :r, param: :hash, only: :show
 
+  get '/p/*path', to: 'pages#show', as: :page
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
